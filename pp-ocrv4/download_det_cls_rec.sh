6fc31c22c5dd046c0e2eddcfca1e68bf526f4175
weijh <imweijh@users.noreply.github.com>
2024-12-16 22:21:31 +0800

    Create download_det_cls_rec.sh

---
+++ pp-ocrv4/download_det_cls_rec.sh
@@ -0,0 +1,3 @@
+wget https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_det_infer.tar
+wget https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
+wget https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_rec_infer.tar
